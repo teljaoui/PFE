aa50c60d56ccc6b239eb307ab27452d8379cff9d
teljaoui <teljaoui2004@gmail.com>
2024-05-30 01:42:39 +0100

    up

--- FrontEnd/src/components/FeaturedCollection.jsx
+++ FrontEnd/src/components/FeaturedCollection.jsx
@@ -115,7 +115,7 @@ export default function FeaturedCollection({ currentProductId, Products }) {
             </div>
         );
     };
-    var settings = {
+    const settings = {
         dots: true,
         infinite: false,
         speed: 500,
@@ -130,11 +130,12 @@ export default function FeaturedCollection({ currentProductId, Products }) {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 4,
-                    initialSlide:1,
+                    slidesToScroll: 1,
+                    initialSlide: 0,
                     infinite: true,
                     dots: true,
-                    nextArrow:null,
-                    prevArrow:null,
+                    nextArrow: <NextArrow />,
+                    prevArrow: <PrevArrow />,
                 }
             },
             {
@@ -142,10 +143,11 @@ export default function FeaturedCollection({ currentProductId, Products }) {
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 1,
+                    initialSlide: 0,
                     infinite: true,
                     dots: true,
-                    nextArrow:null,
-                    prevArrow:null,
+                    nextArrow: <NextArrow />,
+                    prevArrow: <PrevArrow />,
                 }
             },
             {
@@ -153,8 +155,9 @@ export default function FeaturedCollection({ currentProductId, Products }) {
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1,
-                    nextArrow:null,
-                    prevArrow:null,
+                    initialSlide: 0,
+                    nextArrow: <NextArrow />,
+                    prevArrow: <PrevArrow />,
                 }
             },
             {
@@ -162,13 +165,14 @@ export default function FeaturedCollection({ currentProductId, Products }) {
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1,
-                    nextArrow:null,
-                    prevArrow:null,
-                    
+                    initialSlide: 0,
+                    nextArrow: <NextArrow />,
+                    prevArrow: <PrevArrow />,
                 }
             }
         ]
     };
+
     const scroll = () => {
         window.scrollTo({
             top: 0,
